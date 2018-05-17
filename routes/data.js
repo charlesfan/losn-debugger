abf6a609ef3ee835f5789b428d5625ff7e5ac12a
CharlesFan <rock9030@gmail.com>
2018-05-17 19:00:30 +0800

    feature(db): Add select query in mssql

--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router = express.Router();
-
+const mssql = require('../lib/mssql');
+const schema = require('../lib/mssql-schema');
+const sql = require('../lib/sql');
+/*
 router.get('/', function(req, res) {
 	res.send('Herer is Debugger!!!!');
 });
+*/
 
+router.get('/', function(req, res) {
+	let db = new mssql.Server();
+	/*let sql = 'select top 100 * from Fulltable where Station_GUID = @id ORDER BY Checkin_time DESC';
+	let input = [
+		{
+			param: 'id',
+			type: schema.FullTable.Station_GUID,
+			value: 'LOSN-607b21'
+		}
+	]
+	db.query(sql, input).then(result => {
+		res.send(result.recordset);
+	});*/
+	let params = {};
+	if(req.query.Station_GUID) {
+		params.Station_GUID = req.query.Station_GUID;
+	}
+
+	let select = new sql.Select('Fulltable', params);
+	select.top('100').where({Station_GUID: "\'LOSN-607b21\'"}).run(db).then(result => {
+		res.send(result);
+	});
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const bodyParser = require('body-parser');
+
 exports.start = function(app) {
-	app.use('/debug', require('./routes/debug'));
+	app.use('/data', require('./routes/data'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
 	app.use('/', function(req, res) {
 		res.send('losen debugger');
 	});
