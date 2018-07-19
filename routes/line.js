8bc3670c514d7b9b559be3203f11cc4022fda347
CharlesFan <rock9030@gmail.com>
2018-07-19 10:38:05 +0800

    feature(line-api): Add list line info api

--- routes/line.js
+++ routes/line.js
@@ -3,6 +3,9 @@ const util = require('util');
 const router = express.Router();
 const mssql = require('../lib/mssql');
 const respotority = require('../lib/respotority');
+const lineRespotority = require('../models/line');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 
 router.get('/:id/devices', function(req, res) {
 	let db = new mssql.Server();
@@ -33,5 +36,20 @@ router.get('/:id/devices', function(req, res) {
 			res.send(result);
 		});
 	});
+});
+
+router.get('/info/list', async (req, res) => {
+	let db = new mssql.Server();
+	let respotority = new lineRespotority(db);
+
+	try {
+		await respotority.list(req.query).then(result => {
+			res.send(result);
+		});
+	} catch(err) {
+		console.log(err);
+		res.status(err.code);
+		res.send(err);
+	}
 });
 module.exports = router;
